f9a095838f31f507d4965830d5cb5f2c2593d9d5
livlaurel <OLaurel113@gmail.com>
2024-10-20 19:51:55 -0500

    Changing questions to fit storyline

--- src/quizData.ts
+++ src/quizData.ts
@@ -10,34 +10,79 @@ export interface Option {
  
  export const quizData: Question[] = [
    {
-     question: "What would you do on a day off?",
+     question: "You have a massive exam coming up. How are you feeling?",
      options: [
-       { text: "read", value: "bsh" },
-       { text: "nap", value: "ragdoll" },
-       { text: "work out", value: "calico" },
-       { text: "hang with friends", value: "bombay" },
+       { text: "Stressed but i'll work hard!", value: "bsh" },
+       { text: "Sleepyyy", value: "ragdoll" },
+       { text: "AHHH", value: "bombay" },
+       { text: "Theres an exam?", value: "calico" },
      ],
    },
   {
-    question: "You have a big test coming up how do you prepare?",
+    question: "You have one week to prep. How are you planning?",
     options: [
-      { text: "study a bit then nap", value: "ragdoll" },
-      { text: "study with friends", value: "bombay" },
-      { text: "create a study plan", value: "bsh" },
-      { text: "cram the night before", value: "calico" },
+      { text: "Following the study guide", value: "ragdoll" },
+      { text: "Studying with friends", value: "bombay" },
+      { text: "Create a color coded study plan", value: "bsh" },
+      { text: "Cramming the night before", value: "calico" },
     ],
   },
   {
-    question: "You're at a party what do you do?",
+    question: "It's the night before. What are you doing?",
     options: [
-      { text: "dance with friends", value: "bombay" },
-      { text: "stay for a bit to be polite then leave", value: "bsh" },
-      { text: "PARTY!", value: "calico" },
-      { text: "not going im in bed by 8", value: "ragdoll" },
+      { text: "Texing my friends", value: "bombay" },
+      { text: "Looking over notes", value: "bsh" },
+      { text: "Cramming", value: "calico" },
+      { text: "Getting a good nights sleep", value: "ragdoll" },
     ],
   },
   {
-    question: "pick one",
+    question: "5 mins before the test. How do you feel?",
+    options: [
+      { text: "Prepared", value: "bsh" },
+      { text: "Tired", value: "ragdoll" },
+      { text: "It is what it is", value: "calico" },
+      { text: "Freaking out", value: "bombay" },
+    ],
+  },
+  {
+    question: "The test has begun! What do you do first?",
+    options: [
+      { text: "Start with question one", value: "ragdoll" },
+      { text: "Look around to see what everyone else does", value: "bombay" },
+      { text: "Read through the questions", value: "bsh" },
+      { text: "Start on the back", value: "calico" },
+    ],
+  },
+  {
+    question: "You get a question you didn't plan for. What do you do?",
+    options: [
+      { text: "Eeny, meeny, miny, moe", value: "calico" },
+      { text: "Try an educated guess", value: "bsh" },
+      { text: "Pray", value: "bombay" },
+      { text: "Skip it and come back", value: "ragdoll" },
+    ],
+  },
+  {
+    question: "You finished your exam! How do you feel?",
+    options: [
+      { text: "Awful", value: "calico" },
+      { text: "I totally aced it", value: "bsh" },
+      { text: "Ready to nap", value: "ragdoll" },
+      { text: "Relived it's over", value: "bombay" },
+    ],
+  },
+  {
+    question: "You get your grades back and you passed! How do you celebrate?",
+    options: [
+      { text: "Relax at a cafe", value: "bsh" },
+      { text: "Sleep", value: "ragdoll" },
+      { text: "Party!", value: "calico" },
+      { text: "Hanging out with friends", value: "bombay" },
+    ],
+  },
+  {
+    question: "Pick one!",
     options: [
       { text: "≽ܫ≼", value: "calico" },
       { text: "₊˚⊹♡ ᓚ₍ ^. .^₎", value: "bombay" },
@@ -52,24 +97,6 @@ export interface Option {
       { text: "MEOW", value: "calico" },
       { text: "mew", value: "ragdoll" },
       { text: "meoooow", value: "bombay" },
-    ],
-  },
-  {
-    question: "Do you get annoyed easily?",
-    options: [
-      { text: "ehhhhh sometimes", value: "bsh" },
-      { text: "nah", value: "ragdoll" },
-      { text: "duh.", value: "calico" },
-      { text: "mmmmmh", value: "bombay" },
-    ],
-  },
-  {
-    question: "When do you wake up?",
-    options: [
-      { text: "8AM", value: "bsh" },
-      { text: "2PM", value: "ragdoll" },
-      { text: "7AM", value: "calico" },
-      { text: "10AM", value: "bombay" },
     ],
   }
  ];
